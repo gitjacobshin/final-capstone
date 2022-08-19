d46a273f58b8a90b6f88aa37f7ac1b6517f4943d
Gregor Dzierzon <gregor.dzierzon@techelevator.com>
2022-08-19 13:33:25 -0600

    configure image upload

--- src/main/java/com/techelevator/controller/ImageDemoController.java
+++ src/main/java/com/techelevator/controller/ImageDemoController.java
@@ -1,5 +1,6 @@
 package com.techelevator.controller;
 
+import com.techelevator.model.dto.User;
 import com.techelevator.services.uploads.UploadProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpSession;
 
 @Controller
 public class ImageDemoController
@@ -26,16 +29,17 @@ public class ImageDemoController
     }
 
     @RequestMapping(value="/upload", method = RequestMethod.POST)
-    public String uploadImage( @RequestParam(required = false) MultipartFile file )
+    public String uploadImage( @RequestParam(required = false) MultipartFile file, HttpSession session)
     {
         //save restaurant without image and get the restaurantid
+        User currentUser = (User)session.getAttribute("currentUser");
 
         if(file != null && !file.isEmpty())
         {
             try
             {
                 //come up with a file name first
-                String defaultFileName = "1";// should be the resaurant id
+                String defaultFileName = currentUser.getName();
 
 
                 //save the file with the chosen name
